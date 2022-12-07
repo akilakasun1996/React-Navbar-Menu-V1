ab58a30dfed7602b1de1c6646aa7484fed588005
akilakasun1996 <akilaKasun1996@gmail.com>
2022-12-07 22:44:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import React from 'react';
+
+const App = () =>{
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
